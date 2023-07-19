142abfd45bd8fbdc43d7bbadfa7bd0ee63847135
KevinNizet <kevin.nizet@gmail.com>
2023-07-19 11:00:14 +0200

    affichage de tous les pokémon de la BDD OK, affichage du détail de tous les pokémon en cliquant dessus OK

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import axios from "axios";
+
 
 function PokemonList() {
   const [pokemonData, setPokemonData] = useState([]);
@@ -7,14 +9,32 @@ function PokemonList() {
   useEffect(() => {
     axios
       .get(`http://localhost:5001/pokemon`)
-      .then((response) => setPokemonData(console.log(response.data)))
+      .then((response) => setPokemonData(response.data))
       .catch((error) => console.error(error.message));
-
   }, []);
 
-  return <div className="pokemonList">
-    <p>Choisi ton Pokémon </p>
-  </div>;
+  return (
+    <>
+      <div className="pokemonList">
+        <p>Choisis ton Pokémon</p>
+        <div className="pokemonList_map">
+          {pokemonData.map((pokemon) => (
+            <li className="pokemonList_list" key={pokemon.id}>
+              <NavLink to={`/pokemon/${pokemon.id}`}>
+                <div>
+                  <h3>{pokemon.firstname}</h3> 
+                 {/*  <img
+                    src={pokemon.image} 
+                    alt={pokemon.name}
+                  /> */}
+                </div>
+              </NavLink>
+            </li>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PokemonList;
